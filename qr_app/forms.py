b76b4e4f08a10697af0b09d6befe83fea6316e43
gerar <gerar132000ng@gmail.com>
2024-08-04 21:24:42 -0500

    add:interface change

--- qr_app/forms.py
+++ qr_app/forms.py
@@ -12,6 +12,10 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'border border-black rounded p-2'}),
+            'imagen_card': forms.ClearableFileInput(attrs={'class': 'border border-gray-300 rounded p-2'}),
+        }
 
 
 class SignUpForm(UserCreationForm):
