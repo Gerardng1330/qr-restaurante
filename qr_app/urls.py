348763d9c4fd6faf65434cc9709bab3c5c934bc8
gerar <gerar132000ng@gmail.com>
2024-09-22 10:12:18 -0500

    jk

--- qr_app/urls.py
+++ qr_app/urls.py
@@ -8,6 +8,9 @@ from . import views
 urlpatterns = [
     #agregar y elminar cartas
     path('add/', views.add_card, name='add_card'),
+    #eliminar
+    path('add/', views.add_card, name='add_card'),
+
     path('prueba/',views.add_card, name='prueba'),
     path('f2f/',views.f2f, name='f2f'),
     path('delete/<int:card_id>/', views.delete_card, name='delete_card'),
