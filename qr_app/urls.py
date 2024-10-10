2f96e47ee3e70991f6f352348476cc1ce22a7702
gerar <gerar132000ng@gmail.com>
2024-10-10 18:34:54 -0500

    dw

--- qr_app/urls.py
+++ qr_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     #agregar y elminar cartas
     path('add/', views.add_card, name='add_card'),
     #eliminar
+    path('add/', views.add_card, name='add_card'),
 
     path('prueba/',views.add_card, name='prueba'),
     path('f2f/',views.f2f, name='f2f'),
